58f64842fb4e2807c447a671ee983b412edd7fe6
Vikram Kadi <vikram.kadi@openx.com>
2013-08-21 22:29:34 -0700

    removing report module

--- reporting-web/app/report/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-report_mod = Blueprint('report', __name__)
-
-@report_mod.route('/<page>')
-def show_template(page):
-    print 'looking for ' + page
-    return render_template('/report/' + str(page))
-
