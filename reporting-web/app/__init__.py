30b616b1c2d5671d6ffe1ca8ac3ab96b02172286
Vikram Kadi <vikram.kadi@openx.com>
2013-08-21 22:26:40 -0700

    simplifying the example

--- reporting-web/app/__init__.py
+++ reporting-web/app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from app.users.views import users_mod
-from app.report.views import report_mod
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.register_blueprint(users_mod, url_prefix='/users')
-app.register_blueprint(report_mod, url_prefix='/report')
+
 
--- reporting-web/app/users/views.py
+++ reporting-web/app/users/views.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
+from flask import request
 
-users_mod = Blueprint('users', __name__)
+users_mod = Blueprint('report', __name__)
 
-@users_mod.route('/<page>')
-def show_template(page):
-    print 'looking for ' + page
-    return render_template('/users/' + str(page))
+@users_mod.route('/profiles/<id>')
+def show_template(id):
+    print 'looking for the user profile of : ' + str(id)
+    user = {}
+    user['id']=id
+    user['fname']='vikram'
+    user['lname']='kadi'
+    user['phone']='818-232-2354'
+    return render_template('/users/profile.html', user=user)
