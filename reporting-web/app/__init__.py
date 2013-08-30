9dffd5cee389b8d9201ec0dbddcc343b31962028
Tien Truong <tien@Bart.local>
2013-08-29 22:08:07 -0700

    Added studio page

--- reporting-web/app/__init__.py
+++ reporting-web/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from app.users.views import users_mod
+from app.studio.views import studio_main
 
 app = Flask(__name__, static_folder='static')
 app.register_blueprint(users_mod, url_prefix='/users')
-
-
+app.register_blueprint(studio_main, url_prefix='/studio')
