688f6dae94f6f7b9ba97f7b0f75fb1a5d1470227
Tien Truong <tien@Bart.local>
2013-09-07 00:02:48 -0700

    New table.html and table.js

--- reporting-web/app/__init__.py
+++ reporting-web/app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 from app.users.views import users_mod
 from app.studio.views import studio_main
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(users_mod, url_prefix='/users')
 app.register_blueprint(studio_main, url_prefix='/studio')
