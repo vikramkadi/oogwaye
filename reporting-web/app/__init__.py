7add5ca3f9ebcca58edce9cd971df64269e874aa
Vikram Kadi <vikram.kadi@openx.com>
2013-09-10 20:53:12 -0700

    Reorganizing

--- reporting-web/app/__init__.py
+++ reporting-web/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from app.users.views import users_mod
 from app.studio.views import studio_main
+from app.report.views import report_mod
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(users_mod, url_prefix='/users')
 app.register_blueprint(studio_main, url_prefix='/studio')
+app.register_blueprint(report_mod, url_prefix='/report')
---
+++ reporting-web/app/report/views.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+from flask import request
+
+report_mod = Blueprint('report', __name__)
+
+@report_mod.route('/create/step1')
+def create_report_step_1():
+    datasources = {"mysql-dc-1":"MySql DataCenter 1", "vertica-dc-2" :"HP Vertica DataCenter 2"}
+    return render_template('/report/create_step_1.html', ds=datasources)
+
+@report_mod.route('/create/step2')
+def create_report_step_2():
+    return render_template('/report/create_step_2.html')
