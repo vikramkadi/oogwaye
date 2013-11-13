c94171eb483711711a7b147eed9c6811f5699965
Tien Truong <tien@Bart.local>
2013-11-12 22:51:46 -0800

    Added bootstrap-modal lib

--- reporting-web/app/__init__.py
+++ reporting-web/app/__init__.py
@@ -3,7 +3,9 @@ from app.users.views import users_mod
 from app.report.views import report_mod
 import os
 import settings
+"""
 from app.report import datasources
+"""
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(users_mod, url_prefix='/users')
@@ -11,8 +13,10 @@ app.register_blueprint(report_mod, url_prefix='/report')
 
 def initialize(config):
     base_dir = config.base_dir
+    """
     if os.path.exists(os.path.join(base_dir,config.datasources_store)):
         datasources.init_datasources(os.path.join(base_dir,config.datasources_store))
         datasources.init_db_engines(base_dir)
+    """
 
 initialize(settings)
--- reporting-web/app/report/views.py
+++ reporting-web/app/report/views.py
@@ -3,8 +3,10 @@ import jsonpickle
 from flask import Blueprint, render_template
 from flask import request
 from flask import jsonify
+"""
 from app.report import datasources
 from app.report import dbhelper
+"""
 
 report_mod = Blueprint('report', __name__)
 
@@ -17,6 +19,11 @@ def create_report_step_1():
 def create_report_step_2():
     return render_template('/report/table.html')
 
+@report_mod.route('/create/step3')
+def create_report_step_3():
+    return render_template('/report/main2.html')
+
+"""
 @report_mod.route('/create/step2/start', methods=["GET","POST"])
 def create_report_step2_start():
     #result = dbhelper.pull_from_db(request.form['datasource'], request.form['sql'])
@@ -28,3 +35,4 @@ def create_report_step2_pulldata():
     result = dbhelper.pull_from_db(request.form['datasource'], request.form['sql'])
     resp_body = jsonpickle.encode(result, unpicklable=False)
     return flask.Response(resp_body, mimetype='application/json')
+"""
